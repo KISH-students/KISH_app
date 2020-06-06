ce01aeba59fd6142ad92d5b762a624b0d63c9ecf
Hancho1577 <vojougae35@gmail.com>
2020-06-07 02:08:11 +0700

    Initial commit

---
+++ lib/grades.dart
@@ -0,0 +1,11 @@
+class Constants{
+  static const String elementary = '초등';
+  static const String high = '중고등';
+  static const String highest = '12학년';
+
+  static const List<String> choices = <String>[
+    elementary,
+    high,
+    highest
+  ];
+}
