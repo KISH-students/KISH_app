181a4a3e81a37e906c10ef8704e303d0fd8fb0cd
Hancho1577 <vojougae35@gmail.com>
2020-08-15 21:56:36 +0700

    Update api urls (with https)

--- lib/api_links.dart
+++ lib/api_links.dart
@@ -1,4 +1,4 @@
 class ApiLinks{
-  static const String DDAY = "http://ccc1.kro.kr:2020/api/getExamDates";  // 추후 HTTPS로 변경
-  static const String VIEW_COUNT = "http://ccc1.kro.kr:2020/api/getCount";
+  static const String DDAY = "https://kish-dev.kro.kr/api/getExamDates";  // 추후 HTTPS로 변경
+  static const String VIEW_COUNT = "https://kish-dev.kro.kr/api/getCount";
 }
