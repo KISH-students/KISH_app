0ba18b9a444882ec0f474e93270ded54eec251c8
Hancho1577 <vojougae35@gmail.com>
2021-01-25 02:31:14 +0700

    misc: PDF 뷰어의 제목에서의 개행을 허용하지 않습니다.

--- lib/page/pdf_page.dart
+++ lib/page/pdf_page.dart
@@ -32,7 +32,7 @@ class _PdfPageState extends State<PdfPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Center(child: Text("${widget.title}")),
+          title: Center(child: Text(widget.title.replaceAll("\n", " "))),
           backgroundColor: Colors.black12,
         ),
         body: PDF(
