0f113b32049cf096ec9a87a42e702d2262d6913f
Hancho1577 <vojougae35@gmail.com>
2021-10-21 15:59:48 +0700

    부자연스러운 D-Day 그림자 수정
    일부 해상도에서 부자연스러울 수 있는 그림자를 수정합니다.

--- lib/widget/dday_card.dart
+++ lib/widget/dday_card.dart
@@ -56,8 +56,8 @@ class DDayCard extends StatelessWidget {
             boxShadow: [
               BoxShadow(
                 color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 8,
-                blurRadius: 7,
+                spreadRadius: 5,
+                blurRadius: 10,
                 offset: Offset(0, 3), // changes position of shadow
               ),
             ],
